1f018dd4311e1e7e44949a1ff0c639bbb35eeaa2
PoeticCrane <poeticcrane@gmail.com>
2023-03-17 16:27:48 +0300

    ex 57

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import NewCost from './components/NewCost/NewCost';
+import Costs from './components/Costs/costs/Costs';
+import { useState } from 'react';
+const INITIAL_COSTS = [
+  {
+    id: 'c1',
+    date: new Date(2021, 2, 12),
+    name: 'Холодильник',
+    amount: 999.99,
+  },
+  {
+    id: 'c2',
+    date: new Date(2021, 11, 25),
+    name: 'МакБук',
+    amount: 1254.72,
+  },
+  {
+    id: 'c3',
+    date: new Date(2021, 4, 1),
+    name: 'Джинсы',
+    amount: 49.09,
+  },
+];
 
-function App() {
+const App = () => {
+  const [costs, setCosts] = useState(INITIAL_COSTS);
+  const addCostHandler = (costs) => {
+    setCosts((prevCosts) => {
+      console.log(costs);
+      return [costs, ...prevCosts];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <NewCost onAddCost={addCostHandler} />
+      <Costs costs={costs} />
     </div>
   );
-}
+};
 
 export default App;
